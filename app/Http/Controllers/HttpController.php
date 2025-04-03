d7b3dfad44bc7c9705d761a63c9d97f66cd9a968
Aleksandar Atanasovski <aleksandaratanasovski147@gmail.com>
2025-04-03 19:08:02 +0200

    fisinh home

--- app/Http/Controllers/HttpController.php
+++ app/Http/Controllers/HttpController.php
@@ -12,10 +12,18 @@ class HttpController extends Controller
     }
 
     public function getName($symbol) {
-        $pokemon = $this->getApi($symbol)->collect();
-
-
-        $watertype = $pokemon->where('weight', 4)->collect();
+        $pokemon = $this->getApi($symbol)->collect()->take(10);
+        $values = collect();
+        $types = $pokemon->pluck('types')->collect();
+        // foreach($types as $type) {
+        //     foreach ($type as $value) {
+        //         $values->push($value);
+        //     }
+        // }
+            dd($types);
+        $watertype = $pokemon->where(function ($types){
+                 $types->contains('Water');
+        })->collect();
 
         $water = collect();
 
